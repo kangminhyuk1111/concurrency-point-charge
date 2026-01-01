89ce46258f8d5146708305a13f46efd3e2066eab
kangminhyuk1111 <rkdglqkr12@naver.com>
2026-01-01 19:04:12 +0900

    first commit

---
+++ src/main/java/core/concurrencypointcharge/ConcurrencyPointChargeApplication.java
@@ -0,0 +1,13 @@
+package core.concurrencypointcharge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyPointChargeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrencyPointChargeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/core/concurrencypointcharge/ConcurrencyPointChargeApplicationTests.java
@@ -0,0 +1,13 @@
+package core.concurrencypointcharge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencyPointChargeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
